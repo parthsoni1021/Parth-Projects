ffcd87f611d1eac32ca463017780891e6e03d1ca
parthsoni1021 <bkparthsoni@gmail.com>
2025-10-11 11:24:52 +0530

    Day6 done

---
+++ Day6.py
@@ -0,0 +1,22 @@
+print("Hello")  #function - identifiable due to bracket
+print(len("hello"))
+
+
+def my_func():
+    print("Hello")
+
+#triggering/calling function
+
+my_func()   # something can be put in into parenthesis
+
+#Reeborg's world - Hurdle races Practise
+
+a = 6 
+while a > 0:
+    print(a**2)
+    a -= 1
+
+#While loop is used until a condition is met. For loop has no. of iterations fixed
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json 
+
+
