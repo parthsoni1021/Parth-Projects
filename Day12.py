0d3865b27dee32e767e8baee5aef9adaa467a9b3
parth-soni2 <parth.soni2@gds.ey.com>
2025-10-22 14:31:48 +0530

    day13 over

--- Day12.py
+++ Day12.py
@@ -1,42 +1,103 @@
-# Namespaces: Local vs global
+# # Namespaces: Local vs global
 
-enemies = 1
-def increase_enemies():
-    enemies = 2
-    print(f"enemies inside function: {enemies}")  #2
+# enemies = 1
+# def increase_enemies():
+#     enemies = 2
+#     print(f"enemies inside function: {enemies}")  #2
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")    #1
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")    #1
 
-# Local Scope
-def drink_potion():
-    potion_strength = 2
-    print(potion_strength)
 
-drink_potion()
-# print(potion_strength)     Gives error
 
-#Global scope
-player_health = 10
+# # Local Scope
+# # def drink_potion():
+# #     potion_strength = 2
+# #     print(potion_strength)
 
-def drink_potion():
-    potion_strength = 2   # local variable
-    print(player_health)  # global variable
+# drink_potion()
+# # print(potion_strength)     Gives error
+
+# #Global scope
+# player_health = 10
+
+# def drink_potion():
+#     potion_strength = 2   # local variable
+#     print(player_health)  # global variable
     
-drink_potion()    
+# drink_potion()    
 
-# Any thing we name, a variable, function, datastructure, etc has a namespace. It can be global or local
-# Creating a variable inside a function makes it local, but not the case for if/else/for/while loop etc
+# # Any thing we name, a variable, function, datastructure, etc has a namespace. It can be global or local
+# # Creating a variable inside a function makes it local, but not the case for if/else/for/while loop etc
 
-game_level = 5
-ememies = ['a', 'b', 'c']
+# game_level = 5
+# ememies = ['a', 'b', 'c']
 
-def create_enemy():
-    # new_enemy = ""
-    if game_level <= 5:
-        new_enemy = enemies[0]
+# def create_enemy():
+#     # new enemy = ""  # to avoid the linter caution
+#     if game_level <= 5:
+#         new_enemy = enemies[0]
 
-print(new_enemy)
+#     print(new_enemy) 
+
+# # Modifying something within the global scope
+
+# enemies = 1
+
+# def increase_enemies():
+#     enemies = 2
+#     print(f"enemies inside function: {enemies}")  #2
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")    #1
+
+# # In this example, both enemies are completely different objects. The first one is the global variable, while the other is local
+
+# def increase_enemies2():
+#     enemies += 1    #this will show an error
+#     print(enemies)
+
+# increase_enemies2()
+
+# # pip show pylint flake8 ruff # pip install ruff
+
+
+# def increase_enemies2():
+#     global enemies     # This is often not preferred. Maybe we can read it, but modifying isn't recommended
+#     enemies += 1    #this will show an error
+#     print(enemies)
+
+# # Global Constants
+
+# PI = 3.12159
+# GOOGLE_URL = "https://www.google.com"  
+# # A general convention while using global scope to remind that we shouldn't modify this
+
+import random
+
+print("Welcome to the Number Guessing name\n I'm thinking of a number between 1 and 100")
+level = input("Choose a difficulty, 'easy' or 'hard': ")
+if level == 'easy':
+    attempts = 10
+else:                              #elif can be introduced
+    attempts = 5
+
+num = random.randint(1,100)  #both included
+print(num)
+
+while attempts > 0:
+    print(f"Your have {attempts} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+    if guess > num:
+        print("Too high")
+        attempts -= 1
+    elif guess == num:
+        print("You've made a correct guess")
+        break
+    else:
+        print("Too low")
+        attempts -= 1
+
 
 
 
--- Day13.py
+++ Day13.py
@@ -1,4 +1,27 @@
-list1 = [1,2,3,4,5]
-list1[3,] = 
+# Debugging
 
-print(list1)
+# describe the problem
+# Fix the underlined errors
+# print is your friend
+# Run the code often. After each small step, so that you can fix one bug at a time, not when all got compiled.
+# Debugger
+#  breakpoint, step over, step into, step into my code, step out
+
+# Final tip: Take a break if the code is not running after a time. 
+# Still if the code isn't working after a lot of hardwork, is to ask a friend.
+
+
+import random 
+import maths
+
+def mutate(a_list):
+    b_list = []
+    new_item = 0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1,3)
+        new_item = maths.add(new_item, item)
+        b_list.append(new_item)
+    print(b_list)
+
+mutate([1,2,3,5,8,13])
